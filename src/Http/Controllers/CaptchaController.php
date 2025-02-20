af2b9144ce7d1d78f00513d3df89d7a6a3da9dc2
Mario <nemwiper@tiscalinet.it>
2025-02-20 13:02:27 +0100

    Disable sound

--- src/Http/Controllers/CaptchaController.php
+++ src/Http/Controllers/CaptchaController.php
@@ -24,26 +24,26 @@ class CaptchaController extends Controller
         return Response::view('lucbu-laravelcaptcha::imagecaptcha', ['captcha' => $captcha])->header('Content-Type', "image/png");
     }
 
-    public function captchaSound(){
-        $sessionKey = Config::get('lucbu-laravelcaptcha.sessionKey');
-        if(!Session::has($sessionKey)){
-            Captcha::generateCaptcha();
-        }
-
-        $captcha = strtolower(Session::get($sessionKey));
-        $lang = App::getLocale();
-        $path = 'lucbu/laravelcaptcha/sounds/'.$lang.'/';
-        if(!file_exists($path))
-            $path = 'lucbu/laravelcaptcha/sounds/'.Config::get('lucbu-laravelcaptcha.default_language').'/';
-
-        $audio = new MpegAudio();
-        for($i = 0; $i < strlen($captcha); $i++){
-            $c = $captcha[$i];
-            $audio->append(MpegAudio::fromFile($path . $c . '.mp3'));
-        }
-
-        return Response::view('lucbu-laravelcaptcha::soundcaptcha', ['captcha' => $audio->getFrameData(0)])->header('Content-Type', "audio/mpeg");
-    }
+//    public function captchaSound(){
+//        $sessionKey = Config::get('lucbu-laravelcaptcha.sessionKey');
+//        if(!Session::has($sessionKey)){
+//            Captcha::generateCaptcha();
+//        }
+//
+//        $captcha = strtolower(Session::get($sessionKey));
+//        $lang = App::getLocale();
+//        $path = 'lucbu/laravelcaptcha/sounds/'.$lang.'/';
+//        if(!file_exists($path))
+//            $path = 'lucbu/laravelcaptcha/sounds/'.Config::get('lucbu-laravelcaptcha.default_language').'/';
+//
+//        $audio = new MpegAudio();
+//        for($i = 0; $i < strlen($captcha); $i++){
+//            $c = $captcha[$i];
+//            $audio->append(MpegAudio::fromFile($path . $c . '.mp3'));
+//        }
+//
+//        return Response::view('lucbu-laravelcaptcha::soundcaptcha', ['captcha' => $audio->getFrameData(0)])->header('Content-Type', "audio/mpeg");
+//    }
 
     public function captchaUpdate(){
         Captcha::generateCaptcha();
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,6 +4,6 @@ Route::group([
     'middleware' => ['web']
 ], function () {
 	Route::get('lucbu/laravelcaptcha/image', ['as' => 'lucbu.laravelcaptcha.image', 'uses' => 'CaptchaController@captchaImage']);
-	Route::get('lucbu/laravelcaptcha/sound', ['as' => 'lucbu.laravelcaptcha.sound', 'uses' => 'CaptchaController@captchaSound']);
+//	Route::get('lucbu/laravelcaptcha/sound', ['as' => 'lucbu.laravelcaptcha.sound', 'uses' => 'CaptchaController@captchaSound']);
 	Route::get('lucbu/laravelcaptcha/update', ['as' => 'lucbu.laravelcaptcha.update', 'uses' => 'CaptchaController@captchaUpdate']);
 });
